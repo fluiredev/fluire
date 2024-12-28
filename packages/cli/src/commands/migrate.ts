f94dd978725f82aa16ccb59e601d7a084477898a
matheuslanduci <matheuslanduci@gmail.com>
2024-12-28 10:12:30 -0300

    fix: update package names and migrate webhook functionality

--- packages/cli/src/commands/migrate.ts
+++ packages/cli/src/commands/migrate.ts
@@ -9,6 +9,71 @@ export const migrate = new Command()
 	.name('migrate')
 	.description('Migrate your Fluire to the desired PSP')
 
+type Options = {
+	destructive: boolean
+}
+
+async function migrateWebhooks(
+	stripe: Stripe,
+	paths: string[] | string | true,
+	options: Options
+) {
+	try {
+		log('Migrating webhooks...')
+
+		const loadedWebhooks = await loadWebhooks(paths)
+		let registeredWebhooks: Stripe.WebhookEndpoint[] = []
+
+		while (true) {
+			const currentItems = await stripe.webhookEndpoints.list({
+				limit: 100,
+				starting_after: registeredWebhooks[registeredWebhooks.length - 1]?.id
+			})
+
+			registeredWebhooks = registeredWebhooks.concat(currentItems.data)
+
+			if (!currentItems.has_more) break
+		}
+
+		if (options.destructive) {
+			log('Deleting existing webhooks...')
+
+			for (const webhook of registeredWebhooks) {
+				await stripe.webhookEndpoints.del(webhook.id)
+			}
+		}
+
+		for (const webhook of loadedWebhooks) {
+			if (!webhook.url) {
+				danger('Webhook does not have a URL. Skipping...')
+				continue
+			}
+
+			const existingWebhook = registeredWebhooks.find(
+				(registered) => registered.url === webhook.url
+			)
+
+			if (!options.destructive && existingWebhook) {
+				log(`Updating webhook ${webhook.url}...`)
+
+				await webhook.update(existingWebhook.id)
+			} else {
+				log(`Registering webhook ${webhook.url}...`)
+
+				const { secret, id } = await webhook.register()
+
+				log(
+					`Webhook ${id} registered with secret ${secret} - Save it securely!`
+				)
+			}
+		}
+	} catch (error) {
+		if (error instanceof Error) {
+			danger(`Error migrating webhooks: ${error.message}`)
+		}
+	}
+}
+
 export const stripeMigrate = migrate
 	.command('stripe')
 	.description('Migrate your Fluire to Stripe')
@@ -36,56 +101,12 @@ export const stripeMigrate = migrate
 
 			const stripe = new Stripe(config.fluire.stripe.key)
 
+			const promises: Promise<void>[] = []
+
 			if (config.stripe.webhooks) {
-				log('Migrating webhooks...')
-
-				const loadedWebhooks = await loadWebhooks(config.stripe.webhooks)
-				let registeredWebhooks: Stripe.WebhookEndpoint[] = []
-
-				while (true) {
-					const currentItems = await stripe.webhookEndpoints.list({
-						limit: 100,
-						starting_after:
-							registeredWebhooks[registeredWebhooks.length - 1]?.id
-					})
-
-					registeredWebhooks = registeredWebhooks.concat(currentItems.data)
-
-					if (!currentItems.has_more) break
-				}
-
-				if (options.destructive) {
-					log('Deleting existing webhooks...')
-
-					for (const webhook of registeredWebhooks) {
-						await stripe.webhookEndpoints.del(webhook.id)
-					}
-				}
-
-				for (const webhook of loadedWebhooks) {
-					if (!webhook.url) {
-						danger('Webhook does not have a URL. Skipping...')
-						continue
-					}
-
-					const existingWebhook = registeredWebhooks.find(
-						(registered) => registered.url === webhook.url
-					)
-
-					if (!options.destructive && existingWebhook) {
-						log(`Updating webhook ${webhook.url}...`)
-
-						await webhook.update(existingWebhook.id)
-					} else {
-						log(`Registering webhook ${webhook.url}...`)
-
-						const { secret, id } = await webhook.register()
-
-						log(
-							`Webhook ${id} registered with secret ${secret} - Save it securely!`
-						)
-					}
-				}
+				promises.push(migrateWebhooks(stripe, config.stripe.webhooks, options))
 			}
+
+			await Promise.all(promises)
 		}
 	})
